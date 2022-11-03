086498c0e7719c10bdf7e5f48d49e42b21430efa
IT086469 <IT086469@cerner.net>
2022-11-03 22:16:34 +0200

    dockerize rails application

---
+++ dev-docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle check || bundle install
+
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
+
+exec "$@"
